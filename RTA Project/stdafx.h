14719fe65b7938ffe70bb45fcf122da3cbe4310e
Pedro De Jesus <pdejesus01@outlook.com>
2016-06-29 12:21:39 -0400

    Initial setup.

---
+++ RTA Project/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <cstring>
+#include <cstdlib>
+#include <conio.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <algorithm>
+
+#define FBXSDK_SHARED
+#include "fbxsdk.h"
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+using namespace std;
